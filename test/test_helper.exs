7f3c798be59b5ae39764cfccd0982b90b5c18956
Brook <71849503+Primebrook@users.noreply.github.com>
2026-01-22 18:02:41 +0000

    configure and start test endpoint

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,1 +1,11 @@
+# Configure the test endpoint
+Application.put_env(:exunit_openapi, ExUnitOpenAPI.TestApp.Endpoint,
+  http: [port: 4002],
+  server: false,
+  secret_key_base: String.duplicate("a", 64)
+)
+
+# Start the test endpoint
+{:ok, _} = ExUnitOpenAPI.TestApp.Endpoint.start_link()
+
 ExUnit.start()
